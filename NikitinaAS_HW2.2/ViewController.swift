108fcd5d1f6b3f3730da54932c1272696524dac2
Anikasn <anikasn21@gmail.com>
2021-07-09 08:53:09 +0300

    Connect Sliders to Labels

--- NikitinaAS_HW2.2/ViewController.swift
+++ NikitinaAS_HW2.2/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorResultView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        colorResultView.layer.cornerRadius = colorResultView.frame.height / 8
+    }
+    
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(Int(redSlider.value))
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = String(Int(greenSlider.value))
+    }
+    
+    @IBAction func blueSiderAction() {
+        blueValueLabel.text = String(Int(blueSlider.value))
+    }
+    
 }
 
